0487ecf0b5d5bc2339c4fbf63a2dd476a47f209b
cyalc <cyalcinsoy@gmail.com>
2024-08-28 17:12:37 +0200

    Connect RepoDetail and viewmodel data loading

--- feature/repos/src/main/java/com/cyalc/repositories/ui/detail/RepoDetailScreen.kt
+++ feature/repos/src/main/java/com/cyalc/repositories/ui/detail/RepoDetailScreen.kt
@@ -3,14 +3,30 @@ package com.cyalc.repositories.ui.detail
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import com.cyalc.repositories.ui.RepoUiModel
+import org.koin.androidx.compose.koinViewModel
 
 @Composable
-fun RepoDetailScreen(id: Long) {
-    RepoDetail()
+fun RepoDetailScreen(
+    viewModel: RepoDetailViewModel = koinViewModel(),
+    id: Long
+) {
+    LaunchedEffect(key1 = id) {
+        viewModel.loadRepo(id)
+
+    }
+
+    RepoDetail(
+        repoUiModel = viewModel.repo
+            .collectAsState()
+            .value
+    )
 }
 
 @Composable
-fun RepoDetail() {
+fun RepoDetail(repoUiModel: RepoUiModel?) {
     Text(
         text = "Repo Name",
         style = MaterialTheme.typography.titleMedium
