0229917ac4bcbce24a7fbaf8cc8af6823d519a2d
cyalc <cyalcinsoy@gmail.com>
2024-08-28 14:03:58 +0200

    Add fields to domain model for detail page

--- data/repositories/src/main/java/com/cyalc/repositories/Repository.kt
+++ data/repositories/src/main/java/com/cyalc/repositories/Repository.kt
@@ -3,9 +3,12 @@ package com.cyalc.repositories
 data class Repository(
     val id: Long,
     val name: String,
+    val fullName: String,
+    val description: String?,
+    val htmlUrl: String?,
     val ownerAvatarUrl: String,
     val visibility: String,
-    val isPrivate: Boolean
+    val isPrivate: Boolean,
 ) {
     val isPublic: Boolean
         get() = !isPrivate
