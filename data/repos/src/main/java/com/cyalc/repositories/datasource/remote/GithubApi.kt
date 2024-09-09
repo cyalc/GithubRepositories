d961bae2b924fbc9f4d809da20b45436d027b826
cyalc <cyalcinsoy@gmail.com>
2024-09-09 13:55:23 +0200

    Fetch my own repos

--- data/repos/src/main/java/com/cyalc/repositories/datasource/remote/GithubApi.kt
+++ data/repos/src/main/java/com/cyalc/repositories/datasource/remote/GithubApi.kt
@@ -5,7 +5,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 internal interface GithubApi {
-    @GET("users/abnamrocoesd/repos")
+    @GET("users/cyalc/repos")
     suspend fun fetchRepositories(
         @Query("page") page: Int,
         @Query("per_page") size: Int,
